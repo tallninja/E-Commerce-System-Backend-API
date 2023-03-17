d00740f643498d4bd712fe5c95ba9755002e05c6
Ernest Wambua <ernestwambua2@gmail.com>
2023-03-17 11:36:17 +0300

    Tested orders resource

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,6 @@
-export class CreateOrderDto {}
+import { IsDecimal, IsNumber, Min } from 'class-validator';
+
+export class CreateOrderDto {
+  @IsNumber({ maxDecimalPlaces: 2 })
+  total: number;
+}
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { UsersModule } from '../users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forFeature([Order]), UsersModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
